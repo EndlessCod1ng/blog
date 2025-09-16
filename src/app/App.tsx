02fa13c489e9f800a017832fcbfc3dfb20e50a71
EndlessC0ding <endlesscoding@yahoo.com>
2025-09-16 22:41:32 +0300

    cr thms

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,21 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { ProfilePage } from "../pages/ProfilePage";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 interface AppProps {
   className?: string;
 }
+type Theme = "light" | "dark";
 export const App = ({ className }: AppProps) => {
+  const [theme, setTheme] = useState<Theme>("light");
   return (
-    <div className={`${className ? className : ""}`}>
-      <div>App</div>
+    <div className={`app_${theme}_theme ${className ? className : ""}`}>
       <Link to={"/"}>Home</Link>
       <Link to={"/profile"}>Profile</Link>
+      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+        ChangeTHeme
+      </button>
 
       <Suspense fallback="loading...">
         <Routes>
